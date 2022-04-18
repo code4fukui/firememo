9a16df8dc2f6a3027fea180e100b5699bf8d38ce
Taisuke Fukuno <fukuno@jig.jp>
2022-04-19 08:13:54 +0900

    add template

---
+++ firebaseConfig_template.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
